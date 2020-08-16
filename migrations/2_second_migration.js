7b8b2f7ce5aecc7f5c000f0befadd39cadf3f94c
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-08-16 14:05:19 +0200

    Remove Machines contract

--- migrations/2_second_migration.js
+++ migrations/2_second_migration.js
@@ -1,7 +1,6 @@
 var contract = require("@truffle/contract");
 const RoleManager = artifacts.require("RoleManager");
 const Entities = artifacts.require("Entities");
-const Machines = artifacts.require("Machines");
 const Product = artifacts.require("Product");
 const DemoProductionLine = artifacts.require("DemoProductionLine");
 const SupplyLine = artifacts.require("SupplyLine");
@@ -10,7 +9,6 @@ module.exports = function(deployer) {
   deployer.then(async () => {
     var RoleManagerDeployed = await deployer.deploy(RoleManager);
     await deployer.deploy(Entities, RoleManagerDeployed.address);
-    await deployer.deploy(Machines, RoleManagerDeployed.address);
     await deployer.deploy(Product);
     await deployer.deploy(DemoProductionLine);
     await deployer.deploy(SupplyLine);
