0595f3aee5f4724bec97b55255f33935601ecb3c
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-08-20 08:40:09 +0200

    Remove vgr-mqtt-handler

--- clients/VGR/vgr-client.js
+++ clients/VGR/vgr-client.js
@@ -1,13 +1,9 @@
 require('dotenv').config()
 
 const mqtt = require('mqtt');
-
-var VGRMQTTHandler = require('./vgr-mqtt-handler')
 var Web3 = require('web3')
 var ProvidersManager = require('../../utilities/providers-manager')
 var KeyManager = require('../../utilities/keys-manager')
 var ContractManager = require('../../utilities/contracts-manager')
 var Logger = require('../../utilities/logger')
 
-var mqttClient = new VGRMQTTHandler();
-mqttClient.connect();
