3938fc1313a5938eaab397421bc3a2951a12625e
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-08-23 12:59:02 +0200

    Update migration script

--- migrations/2_second_migration.js
+++ migrations/2_second_migration.js
@@ -1,11 +1,6 @@
 
 require('dotenv').config()
 
-var contract = require("@truffle/contract");
-const RoleManager = artifacts.require("RoleManager");
-const Entities = artifacts.require("Entities");
-const Product = artifacts.require("Product");
-const DemoProductionLine = artifacts.require("DemoProductionLine");
 const SupplyLine = artifacts.require("SupplyLine");
 const HBW = artifacts.require("HBW");
 const VGR = artifacts.require("VGR");
@@ -14,10 +9,6 @@ const SLD = artifacts.require("SLD");
 
 module.exports = function(deployer) {
   deployer.then(async () => {
-    var RoleManagerDeployed = await deployer.deploy(RoleManager);
-    await deployer.deploy(Entities, RoleManagerDeployed.address);
-    await deployer.deploy(Product);
-    await deployer.deploy(DemoProductionLine);
     await deployer.deploy(SupplyLine);
     await deployer.deploy(VGR, process.env.ADMIN, process.env.VGR);
     await deployer.deploy(HBW, process.env.ADMIN, process.env.HBW);
