f450037a12683ed61d2d69a9ae1ef2171ded60ac
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-09-02 16:55:23 +0200

    Remove node-red from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,6 @@ IFS=
 
 pkill -f ganache-cli # stop the running instance if
 
-pkill -f node-red # stop node-red
-
 nohup ganache-cli -m $ADMIN_MNEMONIC -e 1000 > /dev/null 2>&1 & # run new blockchain with admin mnemonic
 
 truffle migrate --network $NETWORK # deploy main contracts
@@ -19,5 +17,3 @@ truffle migrate --network $NETWORK # deploy EthereumDIDRegistry contract
 cd ..
 
 node scripts/seed.js # run seed script
-
-node-red
