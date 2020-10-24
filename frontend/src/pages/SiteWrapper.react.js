771bafbde25311f78b9ef6ea98d055ab3ac665b3
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-10-24 20:06:10 +0200

    Remove footer from sitewrapper

--- frontend/src/pages/SiteWrapper.react.js
+++ frontend/src/pages/SiteWrapper.react.js
@@ -117,33 +117,6 @@ class SiteWrapper extends React.Component<Props, State> {
         }}
         navProps={{ itemsObjects: navBarItems }}
         routerContextComponentType={withRouter(RouterContextProvider)}
-        footerProps={{
-          nav: (
-            <React.Fragment>
-              <Grid.Col auto={true}>
-                <List className="list-inline list-inline-dots mb-0">
-                  <List.Item className="list-inline-item">
-                    <a href="./docs/index.html">Documentation</a>
-                  </List.Item>
-                  <List.Item className="list-inline-item">
-                    <a href="./faq.html">FAQ</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col auto={true}>
-                <Button
-                  href="https://github.com/tabler/tabler-react"
-                  size="sm"
-                  outline
-                  color="primary"
-                  RootComponent="a"
-                >
-                  Source code
-                </Button>
-              </Grid.Col>
-            </React.Fragment>
-          ),
-        }}
       >
         {this.props.children}
       </Site.Wrapper>
