3115f1e37d78aa99b11add39e5b03077603025ce
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-11-19 22:43:12 +0100

    Add state clinet run

--- utilities/run-client.js
+++ utilities/run-client.js
@@ -6,7 +6,7 @@ var VGRClient = require("../MQTT/machines/vgr-client");
 var SLDClient = require("../MQTT/machines/sld-client");
 var MPOClient = require("../MQTT/machines/mpo-client");
 var ReadingClient = require("../MQTT/readings-client");
-
+var StateClient = require("../MQTT/state-client");
 new FactorySimulator().connect();
 
 for (var i = 0; i < process.argv.length; i++) {
@@ -28,6 +28,9 @@ for (var i = 0; i < process.argv.length; i++) {
           break;
         case "R":
           client = new ReadingClient();
+          break;
+        case "S":
+          client = new StateClient();
           break;
       }
       client.connect();
