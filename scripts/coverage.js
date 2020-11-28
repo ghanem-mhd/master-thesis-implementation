c67f0473c9e754db948a95e9c3ac661f104e9cba
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2020-11-28 13:41:03 +0100

    Enable solidity test coverage

---
+++ scripts/coverage.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const { runCoverage } = require("@openzeppelin/test-environment");
+
+async function main() {
+  await runCoverage(
+    ["setTypes", "Migration", "processes/MockProcess"],
+    "npm run compile",
+    "./node_modules/.bin/mocha --exit --timeout 10000 --recursive".split(" ")
+  );
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
