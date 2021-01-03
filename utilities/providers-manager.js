4aad50985dd31931f92565b55d976e281d9f371b
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2021-01-03 23:01:53 +0100

    Cleanup truffle config Add dev blockchain scripts

--- utilities/providers-manager.js
+++ utilities/providers-manager.js
@@ -7,28 +7,28 @@ const config = require("../truffle-config");
 module.exports = {
   getWSProvider: function (networkName) {
     switch (networkName) {
-      case "ganache-cli":
-        return new Web3.providers.WebsocketProvider(
-          `ws://${config.networks["ganache-cli"].host}:${config.networks["ganache-cli"].port}`
-        );
       case "quorum":
         return new Web3.providers.WebsocketProvider(
           `ws://${config.networks.quorum.host}:${config.networks.quorum.wsPort}`
+        );
+      default:
+        return new Web3.providers.WebsocketProvider(
+          `ws://${config.networks[networkName].host}:${config.networks[networkName].port}`
         );
     }
   },
   getHttpProvider: function (networkName, privateKeyOrMnemonic) {
     privateKeyOrMnemonic = privateKeyOrMnemonic.replace('"', "");
     switch (networkName) {
-      case "ganache-cli":
-        return new HDWalletProvider(
-          privateKeyOrMnemonic,
-          `http://${config.networks["ganache-cli"].host}:${config.networks["ganache-cli"].port}`
-        );
       case "quorum":
         return new HDWalletProvider(
           privateKeyOrMnemonic,
           `http://${config.networks.quorum.host}:${config.networks.quorum.port}`
+        );
+      default:
+        return new HDWalletProvider(
+          privateKeyOrMnemonic,
+          `http://${config.networks[networkName].host}:${config.networks[networkName].port}`
         );
     }
   },
